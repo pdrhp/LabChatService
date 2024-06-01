5329021f15e5785e773d9f8abde9c57b5061d842
pdrhp <pedrohsp2004@gmail.com>
2024-06-01 17:14:17 -0300

    Feat: create a error handler

---
+++ ChatService/Helper/ResponseHelper.cs
@@ -0,0 +1,20 @@
+﻿using ChatService.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatService.Helper;
+
+public static class ResponseHelper
+{
+    public static IActionResult HandleError(ControllerBase controller, IResponse response)
+    {
+        return response.StatusCode switch
+        {
+            400 => controller.BadRequest(response),
+            401 => controller.Unauthorized(response),
+            403 => controller.Forbid(),
+            404 => controller.NotFound(response),
+            500 => controller.StatusCode(StatusCodes.Status500InternalServerError, response),
+            _ => controller.StatusCode(response.StatusCode, response)
+        };
+    }
+}
