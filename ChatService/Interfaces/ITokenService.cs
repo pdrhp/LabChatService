5ce5fcaff45898934cfafad948e1c7b3b9d74bc0
pdrhp <pedrohsp2004@gmail.com>
2024-06-01 16:48:02 -0300

    fix: fix in ITokenService

---
+++ ChatService/Interfaces/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using ChatService.DTOs;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ChatService.Interfaces;
+
+public interface ITokenService
+{
+    ReadUserSessionDTO GenerateToken(CreateUserSessionDTO userSession);
+
+    bool ValidateToken(string token);
+
+    ReadUserSessionDTO GetUserSession(string token);
+}
