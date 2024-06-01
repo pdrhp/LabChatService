a1f9fd8601e8cfdc097ab2b136e8df28dc10bc87
pdrhp <pedrohsp2004@gmail.com>
2024-06-01 16:47:44 -0300

    fix: fix in responses

--- ChatService/Responses/Responses.cs
+++ ChatService/Responses/Responses.cs
@@ -4,7 +4,7 @@ namespace ChatService.Responses;
 
 public record SuccessResponse(bool Flag, int StatusCode, string Message) : IResponse;
 
-public record SuccessResponse<T>(bool Flag, string Message, int StatusCode, T Data) : IResponse;
+public record SuccessResponse<T>(bool Flag, int StatusCode, string Message,  T Data) : IResponse;
 
 public record ErrorResponse(bool Flag, int StatusCode, string Message) : IResponse;
 
