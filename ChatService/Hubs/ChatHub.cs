2998755bd05798e792092ed86367c9af0191d30e
pdrhp <pedrohsp2004@gmail.com>
2024-06-02 00:55:43 -0300

    feat: started the hub configuration for socket connection

---
+++ ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using ChatService.Data;
+using ChatService.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService.Hubs;
+
+public class ChatHub: Hub
+{
+    private readonly ChatServiceDbContext _context;
+    
+    public ChatHub(ChatServiceDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SendMessage(int groupId, string message)
+    {
+    }
+
+ 
+
+}
