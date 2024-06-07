e24e91983218032c7318f36fe356bae249478fe0
pdrhp <pedrohsp2004@gmail.com>
2024-06-07 09:18:45 -0300

    chore: now user has a profile pic stored in database (JUST FOR STUDY)

--- ChatService/Models/User.cs
+++ ChatService/Models/User.cs
@@ -9,6 +9,8 @@ public class User : IdentityUser
     [MaxLength(100)]
     public string Nome { get; set; }
     
+    public byte[] ProfilePicture { get; set; }
+    public string ProfilePictureType { get; set; }
     public ICollection<ChatRequest> RequestsAsRequester { get; set; }
     public ICollection<ChatRequest> RequestsAsRequested { get; set; }
     public User() : base() { }
