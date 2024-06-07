9d97309e852d3dc2328d01af8c500b4cdb56a4b2
pdrhp <pedrohsp2004@gmail.com>
2024-06-07 09:19:56 -0300

    feat: creation of features of retrieving and uploading a photo

---
+++ ChatService/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using ChatService.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpGet("{userId}/profilePicture")]
+    public async Task<IActionResult> GetProfilePicture(string userId)
+    {
+        var result = await _userService.GetProfilePicture(userId);
+        return File(result.ImageData, result.ImageType);
+    }
+    
+    [HttpPost("{userId}/updateProfilePicture")]
+    public async Task<IActionResult> UpdateProfilePicture(string userId, [FromForm] IFormFile file)
+    {
+        var result = await _userService.UpdateProfilePicture(userId, file);
+        return File(result.ImageData, result.ImageType);
+    }
+}
